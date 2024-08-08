2ee3f0264d1d690c4efe4e1e55ffd549ea96eefd
rizkyharahap <rizkyhh1999@gmail.com>
2024-08-08 09:25:18 +0700

    feat: add test versioning

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,10 +3,17 @@
 # Pull the latest changes from the master branch
 git pull origin master
 
+
+# Get the latest tag from GitHub
+TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+# Print tag for verification
+echo "Deploying version: $TAG"
+
 # Build Docker image with versioning
-VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
-docker build -t username/repository:$VERSION .
+docker build -t username/repository:$TAG .
 
 # Deploy the application using Docker Compose
+export TAG=$TAG
 docker-compose down
 docker-compose up -d
